45952bf5a8c40e05999d6dd4bbf44e5e18b71030
BuildAndDestroy <devreap1@gmail.com>
2019-06-18 09:35:51 -0600

    First fork for blockchain.
    This project is to start building a blockchain,
    and benchmark differences between CPU and GPU.

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Simple Python Blockchain with MINING!
 This is the source code for howCode's simple Python Blockchain.
 
 You can watch the video that accompanies this source code here: https://youtu.be/b81Ib_oYbFk
+
+
+##############################
+# Forked by BuildAndDestroy #
+##############################
+
+
+The end goal here is to learn how block chain works with CPU vs. GPU.
--- blockchain.py
+++ blockchain.py
@@ -1,34 +1,51 @@
+#/usr/bin/env python3
+"""Simple Python Project
+
+This python script goes over how a blockchain works.
+A genesis block starts at zero, then hashes created for each new block.
+
+Original code found here:
+https://github.com/howCodeORG/Simple-Python-Blockchain
+"""
+
 import datetime
 import hashlib
 
-class Block:
-    blockNo = 0
-    data = None
-    next = None
+
+class Block(object):
+    """Everything needed to create a Block."""
     hash = None
-    nonce = 0
-    previous_hash = 0x0
-    timestamp = datetime.datetime.now()
 
     def __init__(self, data):
         self.data = data
+        self.blockNo = 0
+        self.next = None
+        self.nonce = 0
+        self.previous_hash = 0x0
+        self.timestamp = datetime.datetime.now()
 
     def hash(self):
-        h = hashlib.sha256()
-        h.update(
-        str(self.nonce).encode('utf-8') +
-        str(self.data).encode('utf-8') +
-        str(self.previous_hash).encode('utf-8') +
-        str(self.timestamp).encode('utf-8') +
-        str(self.blockNo).encode('utf-8')
+        """Create a hash for each attribute and return."""
+        hash_signature = hashlib.sha256()
+        hash_signature.update(
+            str(self.nonce).encode('utf-8') +
+            str(self.data).encode('utf-8') +
+            str(self.previous_hash).encode('utf-8') +
+            str(self.timestamp).encode('utf-8') +
+            str(self.blockNo).encode('utf-8')
         )
-        return h.hexdigest()
+        return hash_signature.hexdigest()
 
     def __str__(self):
-        return "Block Hash: " + str(self.hash()) + "\nBlockNo: " + str(self.blockNo) + "\nBlock Data: " + str(self.data) + "\nHashes: " + str(self.nonce) + "\n--------------"
+        return 'Block Hash: {}\nBlockNo: {}\nBlock Data: {}\nHashes: {}\n--------------'.format(str(self.hash()), str(self.blockNo), str(self.data), str(self.nonce))
 
-class Blockchain:
 
+class Blockchain(object):
+    """Formula for creating new blocks.
+
+    The Genesis block starts at zero,
+    then each new block is hashed out from the previous block.
+    """
     diff = 20
     maxNonce = 2**32
     target = 2 ** (256-diff)
@@ -37,7 +54,7 @@ class Blockchain:
     dummy = head = block
 
     def add(self, block):
-
+        """Add a new block to the blockchain."""
         block.previous_hash = self.block.hash()
         block.blockNo = self.block.blockNo + 1
 
@@ -45,7 +62,8 @@ class Blockchain:
         self.block = self.block.next
 
     def mine(self, block):
-        for n in range(self.maxNonce):
+        """Mine a new block."""
+        for count in range(self.maxNonce):
             if int(block.hash(), 16) <= self.target:
                 self.add(block)
                 print(block)
@@ -53,11 +71,18 @@ class Blockchain:
             else:
                 block.nonce += 1
 
-blockchain = Blockchain()
 
-for n in range(10):
-    blockchain.mine(Block("Block " + str(n+1)))
+def main():
+    """Create bew blocks on the blockchain."""
+    blockchain = Blockchain()
 
-while blockchain.head != None:
-    print(blockchain.head)
-    blockchain.head = blockchain.head.next
+    for count in range(10):
+        blockchain.mine(Block("Block " + str(count+1)))
+
+    while blockchain.head != None:
+        print(blockchain.head)
+        blockchain.head = blockchain.head.next
+
+
+if __name__ == '__main__':
+    main()
